027dcb7f235f2bcb7ef00ad4d6de3d83789dcefd
max232st <max232st@gmail.com>
2022-10-15 20:36:28 +0300

    Example012_Methods-for

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -38,18 +38,38 @@ int yaer = Method3(); // "yaer" идентификатор переменной 
 //Console.WriteLine(yaer); // используем значение которое нам вернул метод
 
 // 4 Вид  Что-то принимают и что-то возвращают
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
 string Method4(int count, string text)
 {
-    int i = 0;
     string result = String.Empty;
-
-    while (i < count)
+    for (int i = 0; i < count; i++)
     {
-
         result = result + text;
-        i++;
+
     }
     return result;
 }
+
 string res = Method4(10, "z ");
-Console.WriteLine(res);
+//Console.WriteLine(res);
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
