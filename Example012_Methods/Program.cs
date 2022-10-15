251621ca7663d8211678f8033343b66331a33092
max232st <max232st@gmail.com>
2022-10-15 21:32:22 +0300

    Example012_Methods-for-array

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -102,11 +102,44 @@ string Replase(string text, char oldValue, char newValue)
 
     return result;
 }
-string newText = Replase(text, ' ', '|');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replase(newText, 'к', 'К');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replase(newText, 'С', 'с');
-Console.WriteLine(newText);
+// string newText = Replase(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replase(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replase(newText, 'С', 'с');
+// Console.WriteLine(newText);
+Console.Clear();
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
